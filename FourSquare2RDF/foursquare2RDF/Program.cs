0ba1affb303c77167a96e0c0c61fa27896b2f6e2
hadyelsahar <hadyelsahar@gmail.com>
2012-07-21 15:51:10 +0200

    retrieving venues from foursquare and parsing them
    console application to retrieve venues from foursquare and parsing them

---
+++ FourSquare2RDF/foursquare2RDF/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace foursquare2RDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string near = "cairo";
+            string queryKW = "";
+            string queryString = "https://api.foursquare.com/v2/venues/search" + "?near=" + near + "&query=" + queryKW + "&client_id=" + util.clientID + "&client_secret=" + util.clientSecret + "&v=20120101";
+            string s = util.get(queryString);
+            JObject obj = JObject.Parse(s);
+            JToken S1 = obj["response"]["venues"];
+
+            foreach (JToken element in S1)
+            {
+                Console.WriteLine(element["name"]);
+            }
+
+        }
+    }
+}
---
+++ FourSquare2RDF/foursquare2RDF/util.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace foursquare2RDF
+{
+    public static class util
+    {
+        public const string clientID = "SUFGRFKOSKRXC0AE35NHTIDAIGMQEZW4W43CAIAOM0QRDDT4";
+        public const string clientSecret = "24RDEHJ0ARTUSS5EXKPFLIQCOFXKWPGHFLZ15H1CM5WDOV3E";
+        
+        //public const string oauthToken = "JMTEROO2BHMRRKPOGGUBRUBMTZKQY5A4WU4WH4LWC4VZF0BE";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static string get(string url)
+        {
+           
+            string responseString; 
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "GET";
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                 responseString = reader.ReadToEnd();
+            }
+            catch
+            {
+                responseString = null;
+            }
+            return responseString ;
+        }
+
+        public static string post(string url)
+        {
+            string responseString;
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "POST";
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                responseString = reader.ReadToEnd();
+            }
+            catch
+            {
+                responseString = null;
+            }
+            return responseString;
+        }
+    }
+}
