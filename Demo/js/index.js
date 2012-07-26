3f36940d26944dbb8779fc28b5e1300eeaf73f5d
hadyelsahar <hadyelsahar@gmail.com>
2012-07-26 18:49:21 +0200

    website design interface done

---
+++ Demo/js/index.js
@@ -0,0 +1,20 @@
+/// <reference path="/jquery-1.7.2.min.js" />
+/// <reference path="/jquery-1.4.1-vsdoc.js" />
+var statisticsURL = "localhost:8732/Design_Time_Addresses/foursquare2RDF/Service1/json/getStatistics";
+
+function loadStatistics() {
+
+
+
+
+
+}
+
+
+$(document).ready(function () {
+
+
+
+
+
+});
